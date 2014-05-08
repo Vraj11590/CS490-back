d5ca2f9b43b28d0290ace13148aeb7c6616fbad2
vrajesh patel <vj@vj.(none)>
2014-05-08 01:34:11 -0400

    quizzes

--- actions.php
+++ actions.php
@@ -11,30 +11,9 @@
 			// print_r($log);
 			echo json_encode($log);		
 	}
-
-	
-
 	$act = $_POST['action'];
 
 	if($act == "QuestionMakeOE"){
-
-//		echo json_encode($_POST);
-
-		// $data = array(
-		//           	'action' =>  'QuestionMakeOE',
-		// 				'Description' => $Description,
-		// 				'T1'=> $t1,
-		// 				'T2'=> $t2,
-		// 				'T3'=> $t3,
-		// 				'T4'=> $t4,
-		// 				'Difficulty'=>$difficulty,
-		// 				'CreatorName'=>$createBy_Name,
-		// 				'CreatorUCID'=>$createdBy,
-		// 				'Question_name'=>$unique_name,
-		// 				'helpcode'=>$helpcode
-		// 			 );
-
-
 		$Question = $_POST['Description'];
 		$t1 = $_POST['T1'];
 		$t2 = $_POST['T2'];
@@ -67,7 +46,6 @@
 			$a = array("Flag" => "Failed" , "Message" => "This question already exists!");
 			echo json_encode($a);
 		}
-
 	}
 
 	if($act == "QuestionMakeMC"){
@@ -131,6 +109,122 @@
 		}		
 	}
 
+	if($act == "QuestionMakeTF"){
+		$Description=$_POST['Description'];
+		$correct = $_POST['Correct'];
+		$difficulty = $_POST['Difficulty'];
+		$createdBy = $_POST['CreatorUCID'];
+		$type = "TF";
+		$unique_name = $_POST['Question_name'];	
+
+		$result = mysqli_query($con,"SELECT * FROM Questions WHERE Question = '$Description'");
+		$check_person = mysqli_num_rows($result);
+
+		if ($check_person == 0){
+			$insert_question = mysqli_query($con,"INSERT INTO Questions (Question, Question_name, Type, Difficulty, CreatedBy) VALUES ('$Description', '$unique_name', '$type', '$difficulty', '$createdBy')"); 
+			$lastId = mysqli_insert_id($con);
+
+			$insert_answer = mysqli_query($con,"INSERT INTO TFAnswers (question_id, correct) VALUES ('$lastId', '$correct')");
+			$a = array("Question" => "Success" , "Message" => "Question added");
+			echo json_encode($a);
+		}
+		if ( $check_person >= 1){
+			$a = array("Flag" => "Failed" , "Message" => "This question already exists!");
+			echo json_encode($a);
+		}		
+	}
+	if($act == "QuestionMakeFB"){
+		$Description=$_POST['Description'];
+		$correct = $_POST['Correct'];
+		$difficulty = $_POST['Difficulty'];
+		$createdBy = $_POST['CreatorUCID'];
+		$type = "FB";
+		$unique_name = $_POST['Question_name'];	
+
+		$result = mysqli_query($con,"SELECT * FROM Questions WHERE Question = '$Description'");
+		$check_person = mysqli_num_rows($result);
+
+		if ($check_person == 0){
+			$insert_question = mysqli_query($con,"INSERT INTO Questions (Question, Question_name, Type, Difficulty, CreatedBy) VALUES ('$Description', '$unique_name', '$type', '$difficulty', '$createdBy')"); 
+			$lastId = mysqli_insert_id($con);
+
+			$insert_answer = mysqli_query($con,"INSERT INTO FBAnswers (question_id, correct) VALUES ('$lastId', '$correct')");
+			$a = array("Flag" => "Success" , "Message" => "Question added");
+			echo json_encode($a);
+		}
+		if ( $check_person >= 1){
+			$a = array("Flag" => "Failed" , "Message" => "This question already exists!");
+			echo json_encode($a);
+		}		
+	}
+	if($act == "send_all_quizzes"){
+		$result = mysqli_query($con,"SELECT quiz_id, Quiz_name, Created_by FROM Quiz");
+
+		$quiz = array();
+		//$answers = array();
+		$i=0;
+
+		while($row = mysqli_fetch_assoc($result)) {
+
+			  $quiz[''.$i] = $row;
+			  
+		      $i++;
+		}   
+
+		echo json_encode($quiz); 
+	}
+	if($act == "send_quiz_by_id"){
+		$con = mysqli_connect($host,$db_UCID,$db_Pass,$dbname);
+
+		$quiz_id = $_POST['quiz_id'];
+
+		$result = mysqli_query($con, "SELECT question_id FROM Quiz WHERE quiz_id = '$quiz_id'");
+
+		$x = array();
+
+
+		while($row = mysqli_fetch_assoc($result)) {
+
+    		$ques_id = $row['question_id'];
+    		$array = explode(',', $ques_id); //split string into array seperated by ', '
+    		$i=0;
+   
+	   		foreach($array as $k => $v){
+
+	       		$result3 = mysqli_query($con, "SELECT Type FROM Questions WHERE QuestionID = '$array[$k]'");
+	       		$row1 = mysqli_fetch_assoc($result3);
+
+	       		if ($row1['Type'] == 'OE'){
+	           		$result2 = mysqli_query($con, "SELECT question_id, Type, Question, T1, T2, T3, T4, HelpCode FROM Questions, OEAnswers WHERE Questions.QuestionID = '$array[$k]' AND Questions.QuestionID =  OEAnswers.question_id");
+	           		$individual = mysqli_fetch_assoc($result2);
+	           		//print_r($individual);
+	           		$x[''.$k] = $individual;
+	       		}
+	       		if ($row1['Type'] == 'MC'){
+	           		$result2 = mysqli_query($con, "SELECT question_id, Type, Question, C1, C2, C3, C4 FROM Questions, MCAnswers WHERE Questions.QuestionID = '$array[$k]' AND Questions.QuestionID =  MCAnswers.question_id");
+	           		$individual = mysqli_fetch_assoc($result2);
+	           		//print_r($individual);
+	           		$x[''.$k] = $individual;
+	       		}   		
+	         	if ($row1['Type'] == 'TF'){
+	           		$result2 = mysqli_query($con, "SELECT question_id, Type, Question FROM Questions, TFAnswers WHERE Questions.QuestionID = '$array[$k]' AND Questions.QuestionID =  TFAnswers.question_id");
+	           		$individual = mysqli_fetch_assoc($result2);
+	           		//print_r($individual);
+	           		$x[''.$k] = $individual;
+	       		}  
+	       		if ($row1['Type'] == 'FB'){
+	           		$result2 = mysqli_query($con, "SELECT question_id, Type, Question FROM Questions, FBAnswers WHERE Questions.QuestionID = '$array[$k]' AND Questions.QuestionID =  FBAnswers.question_id");
+	           		$individual = mysqli_fetch_assoc($result2);
+	           		//print_r($individual);
+	           		$x[''.$k] = $individual;
+	       		}   
+			}
+
+
+		}   		
+      echo json_encode($x);		
+
+	}
 
 
 
--- takequiz.php
+++ takequiz.php
@@ -1,15 +1,11 @@
 <?php
+  include 'dbglobals.php';
 
-$db_UCID = "sjt5";
-$db_Pass = "songbag22";
-$host = "sql2.njit.edu";
-$dbname = "sjt5";
-	
+
 //Connect to the database
 $con = mysqli_connect($host,$db_UCID,$db_Pass,$dbname);
 
-$quiz_id = $_POST['quiz_id'];
-
+//$quiz_id = $_POST['quiz_id'];
 
 $result = mysqli_query($con, "SELECT question_id FROM Quiz WHERE quiz_id = '$quiz_id'");
 
@@ -18,27 +14,44 @@ $x = array();
 
 while($row = mysqli_fetch_assoc($result)) {
 
-	  $ques_id = $row['question_id'];
-      $array = explode(',', $ques_id); //split string into array seperated by ', '
-$i=0;
+    $ques_id = $row['question_id'];
+    $array = explode(',', $ques_id); //split string into array seperated by ', '
+    $i=0;
+   //echo $ques_id;
+   
+   foreach($array as $k => $v){
 
+       $result3 = mysqli_query($con, "SELECT Type FROM Questions WHERE QuestionID = '$array[$k]'");
+       $row1 = mysqli_fetch_assoc($result3);
 
-      foreach($array as $k => $v){
-  		$result2 = mysqli_query($con, "SELECT question_id, Type, Question, GROUP_CONCAT(answers,'') AS answers FROM Questions, Answers WHERE Questions.id = '$array[$k]' AND Questions.id =  Answers.question_id");
-
-    	 $individual = mysqli_fetch_assoc($result2);
-    	//print_r($individual);
-    	   $x[''.$k] = $individual;
-
-
-
-       	$i++;
-      }
+       if ($row1['Type'] == 'OE'){
+           $result2 = mysqli_query($con, "SELECT question_id, Type, Question, T1, T2, T3, T4, HelpCode FROM Questions, OEAnswers WHERE Questions.QuestionID = '$array[$k]' AND Questions.QuestionID =  OEAnswers.question_id");
+           $individual = mysqli_fetch_assoc($result2);
+           //print_r($individual);
+           $x[''.$k] = $individual;
+       }
+       if ($row1['Type'] == 'MC'){
+           $result2 = mysqli_query($con, "SELECT question_id, Type, Question, C1, C2, C3, C4 FROM Questions, MCAnswers WHERE Questions.QuestionID = '$array[$k]' AND Questions.QuestionID =  MCAnswers.question_id");
+           $individual = mysqli_fetch_assoc($result2);
+           //print_r($individual);
+           $x[''.$k] = $individual;
+       }   
+         if ($row1['Type'] == 'TF'){
+           $result2 = mysqli_query($con, "SELECT question_id, Type, Question FROM Questions, TFAnswers WHERE Questions.QuestionID = '$array[$k]' AND Questions.QuestionID =  TFAnswers.question_id");
+           $individual = mysqli_fetch_assoc($result2);
+           //print_r($individual);
+           $x[''.$k] = $individual;
+       }  
+       if ($row1['Type'] == 'FB'){
+           $result2 = mysqli_query($con, "SELECT question_id, Type, Question FROM Questions, FBAnswers WHERE Questions.QuestionID = '$array[$k]' AND Questions.QuestionID =  FBAnswers.question_id");
+           $individual = mysqli_fetch_assoc($result2);
+           //print_r($individual);
+           $x[''.$k] = $individual;
+       }   
+   }
 
 
 }   
-        echo json_encode($x);
-
-
+      echo json_encode($x);
 
 ?>
